bb3e97da1cf7b95378b27152f84b9b6ba28c426b
Sergey Bulekov <s_bulekov@mail.ru>
2023-01-15 20:19:17 +0300

    Commit

---
+++ src/components/FEAT__POPUP.jsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+import {} from '../components/FEAT__POPUP.scss'
+
+
+
+function FEAT__POPUP(props) {
+
+        const closePopup = (_event) => {
+            props.setTrigger(false)
+        };
+    return (props.trigger) ? (
+        <div className="feat__popup" >
+            <div className="feat__btn" >        
+                <button className="close__btn" onClick={closePopup} >x</button>                
+                {props.children}
+            </div> 
+        </div>
+
+    ) : "";
+
+}
+
+export default FEAT__POPUP;
